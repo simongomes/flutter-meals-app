90a0650173072ef64e0cdf30d377edfa9c97c41f
Simon Gomes <busy.s.simon@gmail.com>
2020-08-30 19:27:28 +0600

    Top TabBarView added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_meals_app/screens/tabs_screen.dart';
 import './screens/meal_detail_screen.dart';
 import './screens/categories_screen.dart';
 import './screens/category_meals_screen.dart';
@@ -33,7 +34,7 @@ class MyApp extends StatelessWidget {
 //      home: CategoriesScreen(),
     initialRoute: '/',
       routes: {
-        '/': (context) => CategoriesScreen(),
+        '/': (context) => TabsScreen(),
         CategoryMealsScreen.routeName: (context) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (context) => MealDetailScreen(),
       },
--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -5,24 +5,21 @@ import '../data/dummy_data.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Daily Meal"),),
-      body: GridView(
-        padding: EdgeInsets.all(25),
-        children: DUMMY_CATEGORIES
-            .map((categoryData) =>
-                CategoryItem(
-                  categoryData.id,
-                    categoryData.title,
-                    categoryData.color
-                )
-        ).toList(),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
+    return GridView(
+      padding: EdgeInsets.all(25),
+      children: DUMMY_CATEGORIES
+          .map((categoryData) =>
+          CategoryItem(
+              categoryData.id,
+              categoryData.title,
+              categoryData.color
+          )
+      ).toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
     );
   }
