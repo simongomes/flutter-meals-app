93292bdeeb9546bbc6374fb23182bb4efd94d9f1
Simon Gomes <busy.s.simon@gmail.com>
2020-08-31 19:56:39 +0600

    Drawer item link added, screen replacement functionality added instead of stacking the screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_meals_app/screens/tabs_screen.dart';
+import './screens/tabs_screen.dart';
 import './screens/meal_detail_screen.dart';
-import './screens/categories_screen.dart';
 import './screens/category_meals_screen.dart';
+import './screens/filters_screen.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -37,6 +38,7 @@ class MyApp extends StatelessWidget {
         '/': (context) => TabsScreen(),
         CategoryMealsScreen.routeName: (context) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (context) => MealDetailScreen(),
+        FiltersScreen.routeName: (context) => FiltersScreen(),
       },
     );
   }
--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -1,40 +1,52 @@
 import 'package:flutter/material.dart';
+import '../screens/filters_screen.dart';
 
 class MainDrawer extends StatelessWidget {
-  Widget buildListTile(String title, IconData iconData) {
+  Widget buildListTile(String title, IconData iconData, Function tapHandler) {
     return ListTile(
       leading: Icon(iconData),
-      title: Text(title, style: TextStyle(
-        fontFamily: "RobotoCondensed",
-        fontSize: 20,
-        fontWeight: FontWeight.bold,
-      ),),
-      onTap: () {
-        //...
-      },
+      title: Text(
+        title,
+        style: TextStyle(
+          fontFamily: "RobotoCondensed",
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      onTap: tapHandler,
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Column(children: <Widget>[
-        Container(
-          height: 120,
-          width: double.infinity,
-          padding: EdgeInsets.all(20),
-          alignment: Alignment.centerLeft,
-          color: Theme.of(context).accentColor,
-          child: Text("Cooking Up!", style: TextStyle(
-            fontWeight: FontWeight.w900,
-            fontSize: 25,
-            color: Theme.of(context).primaryColor
-          ),),
-        ),
-        SizedBox(height: 20,),
-        buildListTile("Meals", Icons.restaurant),
-        buildListTile("Filters", Icons.settings),
-      ],),
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 120,
+            width: double.infinity,
+            padding: EdgeInsets.all(20),
+            alignment: Alignment.centerLeft,
+            color: Theme.of(context).accentColor,
+            child: Text(
+              "Cooking Up!",
+              style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 25,
+                  color: Theme.of(context).primaryColor),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          buildListTile("Meals", Icons.restaurant, () {
+            Navigator.of(context).pushReplacementNamed("/");
+          }),
+          buildListTile("Filters", Icons.settings, () {
+            Navigator.of(context).pushReplacementNamed(FiltersScreen.routeName);
+          }),
+        ],
+      ),
     );
   }
 }
